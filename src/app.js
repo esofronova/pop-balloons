c2be9ef9e2cca2d8c6fc146d0d372e2f798aa367
esofronova <essofronova@gmail.com>
2022-01-20 09:19:13 -0800

    fixing balloons size on mobile

--- src/app.js
+++ src/app.js
@@ -1,60 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import './style.scss';
 
 export default function PopBalloons() {
 
    const colors = ['#FF0066', '#00CCFF', '#FFFF66', '#FF66FF', '#0066FF', '#FF6600', '#00FF33', '#FF3300', '#FF0099', '#00FFCC', '#990099', '#339999', '#00FF66', '#00FFFF', '#FF6600', '#FF00CC', '#00FF99', '#FF0000', '#FF33FF', '#33CC33', '#00CCFF', '#FF6699', '#FF00FF', '#66CC33', '#66CC66', '#9900CC', '#660066', '#FF99CC', '#CC0000', '#33FFCC', '#CC33CC', '#3399FF'];
 
-   const balloons = Array.from({ length: 25 }, (k, i) => i++);
+   const balloons = Array.from({ length: 24 }, (k, i) => i++);
    let popped = 0;
-   let circles, gameover;
+
+   let [gameOver, setGameOver] = useState(false);
 
    useEffect(() => {
       document.querySelectorAll('.circle').forEach((item) => {
          item.addEventListener("mouseover", popBallon, { once: true });
+         item.style.height = item.clientWidth + "px"
       });
-      // eslint-disable-next-line
-      circles = document.querySelector('.circles').classList;
-      // eslint-disable-next-line
-      gameover = document.querySelector('.gameover').classList;
    });
 
-   const popBallon = (e) => {
+   const popBallon = e => {
       const color = colors[Math.floor(Math.random() * colors.length)];
       e.target.style.background = "transparent";
       e.target.style.color = color;
       popped++;
-      if (popped === balloons.length) {
-         circles.remove('d-flex');
-         circles.add('d-none');
-         gameover.remove('d-none');
-      };
+      popped === balloons.length && setGameOver(true);
    };
 
    return (
       <div className="pop-balloons py-5">
-         <div className="gradient-text">
-            <h1 className="header">Pop Balloons!</h1>
-            <p className="text-center">Hover over the balloon to pop it</p>
+         <div className="gradient-text text-center text-white mb-5">
+            <h1>Pop Balloons!</h1>
+            <p>Hover over the balloon to pop it</p>
          </div>
-         <div className="circles d-flex flex-wrap mx-auto">
+         <Row className={"circles row mx-auto justify-content-center " + (gameOver ? "d-none" : "d-flex")}>
             {balloons.map((item, index) => {
                const color = colors[Math.floor(Math.random() * colors.length)];
                return (
-                  <div
-                     key={index}
-                     className="circle rounded-circle centered m-2 fs-3 fw-bold pointer"
-                     style={{ background: `radial-gradient(circle at 35px 35px, ${color}, #000)`, color: 'transparent' }}
-                  >POP!</div>
+                  <Col xs={3} md={2} key={index} className="p-2">
+                     <div
+                        className="circle rounded-circle centered fs-3 fw-bold pointer"
+                        style={{ background: `radial-gradient(circle at 35px 35px, ${color}, #000)`, color: 'transparent' }}
+                     >POP!</div>
+                  </Col>
                );
             })}
-         </div>
-         <div className="gameover d-none text-center text-white w-75 mx-auto mt-5">Congratulations! You popped all the balloons!</div>
+         </Row>
+         <div className={"gameover text-center text-white w-100 mt-5 " + (gameOver ? "d-block" : "d-none")}>Congratulations! You popped all the balloons!</div>
          <button
             className="btn p-3 mt-5 d-flex mx-auto start-again fs-3 text-white"
-            onClick={() => {
-               window.location.reload();
-            }}
+            onClick={() => window.location.reload()}
          >Start Again</button>
       </div>
    );
